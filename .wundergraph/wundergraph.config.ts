c0d195fda8434243e65b70e9780f68777f0506c2
Tim Farley <tim@imperfectproduce.com>
2022-10-28 15:55:54 -0400

    initial commit

---
+++ .wundergraph/wundergraph.config.ts
@@ -0,0 +1,65 @@
+import {
+  Application,
+  configureWunderGraphApplication,
+  cors,
+  EnvironmentVariable,
+  introspect,
+  templates,
+} from "@wundergraph/sdk";
+import server from "./wundergraph.server";
+import operations from "./wundergraph.operations";
+
+// Stripe's public-facing OpenAPI spec
+const stripe = introspect.openApi({
+  apiNamespace: "stripe",
+  source: {
+    kind: "file",
+    filePath: "spec3.json",
+  },
+  baseURL: "https://raw.githubusercontent.com/stripe/openapi/master/openapi/",
+});
+
+const myApplication = new Application({
+  name: "app",
+  apis: [stripe],
+});
+
+// configureWunderGraph emits the configuration
+configureWunderGraphApplication({
+  application: myApplication,
+  server,
+  operations,
+  codeGenerators: [
+    {
+      templates: [
+        // use all the typescript react templates to generate a client
+        ...templates.typescript.all,
+        templates.typescript.operations,
+        templates.typescript.linkBuilder,
+      ],
+      // create-react-app expects all code to be inside /src
+      // path: "../frontend/src/generated",
+    },
+  ],
+  cors: {
+    ...cors.allowAll,
+    allowedOrigins:
+      process.env.NODE_ENV === "production"
+        ? [
+            // change this before deploying to production to the actual domain where you're deploying your app
+            "http://localhost:3000",
+          ]
+        : [
+            "http://localhost:3000",
+            new EnvironmentVariable("WG_ALLOWED_ORIGIN"),
+          ],
+  },
+  dotGraphQLConfig: {
+    hasDotWunderGraphDirectory: false,
+  },
+  security: {
+    enableGraphQLEndpoint:
+      process.env.NODE_ENV !== "production" ||
+      process.env.GITPOD_WORKSPACE_ID !== undefined,
+  },
+});
